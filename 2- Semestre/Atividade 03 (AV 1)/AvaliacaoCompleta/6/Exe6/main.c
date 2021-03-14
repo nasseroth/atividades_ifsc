271f5b7773975f3be768bf190a12dcb608c29758
Nasser Othman <nothman09@gmail.com>
2021-03-14 18:58:57 -0300

    Atividade 6
    Atividade 6

--- 2- Semestre/Atividade 03 (AV 1)/AvaliacaoCompleta/6/Exe6/main.c
+++ 2- Semestre/Atividade 03 (AV 1)/AvaliacaoCompleta/6/Exe6/main.c
@@ -1,64 +1,106 @@
 #include <stdio.h>
-
 #include <stdlib.h>
-
-#include <ctype.h>
-
-#include <stdbool.h>
-
-#ifdef __unix__#include <unistd.h>
-
-#include <stdlib.h>
-
-#elif defined(_WIN32) || defined(WIN32)
-
-#define OS_Windows
-
 #include <windows.h>
 
-#endif
-/*
-7) Implemente uma aplicação que realize o cadastro de veículos.
-a) Você deve criar uma estrutura para armazenar os dados do veículo
-contendo (modelo, cor, placa, tipo, característica e status);
-b) Sua aplicação deve conter um menu com as seguintes opções: (inserir,
-editar, remover, listar, pesquisar, sair);
-c) Utilize uma enumeração para representar a cor do veículo, você deve usar
-os seguintes dados: (1 - Preto, 2 - Branco, 3 - Verde, 4 - Azul, 5 - Vermelho,
-6 - Amarelo, 7 - Prata, 8 - Cinza);
-d) Para cadastro do tipo, deve ser informado se o veículo é (1 - Transporte de
-passageiros, 2 - Transporte de Cargas, 3 - Passeio);
-e) Conforme o tipo de veículo, deve ser armazenado uma característica
-diferente no referido campo. Para isto, você deve criar uma união entre:
-quantidade de passageiros (inteiro); carga máxima (float); possui seguro
-(texto). Caso o veículo seja do tipo transporte de passageiros deve ser
-informado a quantidade de passageiros, caso seja transporte de carga, deve
-ser informado a carga máxima e caso contrário, se o mesmo possui seguro
-(sim ou não);
-f) Deve ser possível armazenar 15 registros de veículos. A cada inserção deve
-ser verificado se há posições livres na lista, caso contrário deve ser
-informado que a mesma está cheia.
-g) Quando selecionada a opção de remover usuário, deve ser informada a
-posição que se deseja remover e assim atualizar o status da posição da
-lista. ‘*’ indica que o registro está apagado. ‘+’ indica que o registro está em
-uso. Qualquer outro símbolo indica que o registro está disponível.
-h) Para alteração o usuário deve informar o código desejado, caso a posição
-esteja vazia não deve ser possível alterar o conteúdo;
-i) A lista deve exibir na tela o Código, modelo, marca e placa dos veículos
-cadastrados;
-j) Na opção de pesquisa o usuário informa a posição desejada e são
-impressos todos os dados do veículo.
+int exibeMenu();
+void cadastraVenda(double vendas[5][5][4]);
+void totais(double vendas[5][5][4]);
+unsigned int cp = 1252;
 
-*/
-typedef struct {
-  char nome[50];
-  int numeroVendedor;
-  int idade;
+int main(){
+    SetConsoleCP(cp);
+    SetConsoleOutputCP(cp);
+    double vendas[5][5][4] = {0};
+    int opcao = 0;
+
+    do{
+        opcao = exibeMenu();
+
+        switch(opcao){
+            case 1:
+                cadastraVenda(vendas);
+                system("pause");
+            break;
+            case 2:
+                totais(vendas);
+                system("pause");
+            break;
+        };
+    }while(opcao != 3);
+
 }
-ALUNO;
-ALUNO al[10];
-main()
-{
-    printf("Hello world!\n");
 
+int exibeMenu(){
+    int opcao;
+    system("cls");
+    printf("-- -- -- Menu de Vendas -- -- -- \n");
+    printf("1 - Cadastrar Venda \n");
+    printf("2 - Mostrar Relatorio de vendas\n");
+    printf("3 - Sair \n");
+    printf("-- -- -- -- -- -- -- -- -- -- -- \n");
+    printf("Escolha uma opcao \n");
+    scanf("%d", &opcao);
+    return opcao;
+}
+
+void cadastraVenda(double vendas[5][5][4]){
+    int vendedor;
+    int produto;
+    int dia;
+    double valor;
+
+    printf("Informe o Vendendor (1 à 4):");
+    scanf("%d", &vendedor);
+    vendedor = vendedor - 1;
+
+    printf("Informe o Produto (1 à 5):");
+    scanf("%d", &produto);
+    produto = produto - 1;
+
+    printf("Informe o dia da Venda (1 à 5):");
+    scanf("%d", &dia);
+    dia = dia - 1;
+
+    printf("Valor da Venda:");
+    scanf("%lf", &valor);
+
+    vendas[dia][produto][vendedor] = valor;
+}
+
+void totais(double vendas[5][5][4]){
+    double totais[6][5] = {0};
+
+    int a;
+    int b;
+    int y;
+
+    for(a = 0; a < 5; a++){
+        for(b = 0; b < 5; b++){
+            totais[b][0] += vendas[a][b][0];
+            totais[b][1] += vendas[a][b][1];
+            totais[b][2] += vendas[a][b][2];
+            totais[b][3] += vendas[a][b][3];
+        }
+    }
+
+        totais[0][4] = totais[0][0] + totais[0][1] + totais[0][2] + totais[0][3];
+        totais[1][4] = totais[1][0] + totais[1][1] + totais[1][2] + totais[1][3];
+        totais[2][4] = totais[2][0] + totais[2][1] + totais[2][2] + totais[2][3];
+        totais[3][4] = totais[3][0] + totais[3][1] + totais[3][2] + totais[3][3];
+        totais[4][4] = totais[4][0] + totais[4][1] + totais[4][2] + totais[4][3];
+
+        totais[5][0] = totais[0][0] + totais[1][0] + totais[2][0] + totais[3][0];
+        totais[5][1] = totais[0][1] + totais[1][1] + totais[2][1] + totais[3][1];
+        totais[5][2] = totais[0][2] + totais[1][2] + totais[2][2] + totais[3][2];
+        totais[5][3] = totais[0][3] + totais[1][3] + totais[2][3] + totais[3][3];
+
+    printf("\t1 Equipe\t 2 Equipe\t 3 Equipe\t 4 Equipe\t Totais \n");
+
+        printf("Produto 1: %lf \t %lf \t %lf \t %lf \t %lf \n", totais[0][0], totais[0][1], totais[0][2], totais[0][3], totais[0][4]);
+        printf("Produto 2: %lf \t %lf \t %lf \t %lf \t %lf \n", totais[1][0], totais[1][1], totais[1][2], totais[1][3], totais[1][4]);
+        printf("Produto 3: %lf \t %lf \t %lf \t %lf \t %lf \n", totais[2][0], totais[2][1], totais[2][2], totais[2][3], totais[2][4]);
+        printf("Produto 4: %lf \t %lf \t %lf \t %lf \t %lf \n", totais[3][0], totais[3][1], totais[3][2], totais[3][3], totais[3][4]);
+
+
+    printf("  Totais: %lf \t %lf \t %lf \t %lf \t %lf \n", totais[5][0], totais[5][1], totais[5][2], totais[5][3], totais[5][4]);
 }
